c5eaa30769c8c29a25fcd90f588c94283a4a2a3b
Yaroslav <yarik.lev7@gmail.com>
2019-01-21 21:05:05 +0200

    Intitial1

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,8 @@
 'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-
-module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the flawless ${chalk.red('generator-cscart-module')} generator!`)
-    );
-
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
-
-  install() {
-    this.installDependencies();
-  }
-};
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var OnepageGenerator = yeoman.generators.Base.extend({
+});
+module.exports = OnepageGenerator;
